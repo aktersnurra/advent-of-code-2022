4123a75d465755690ba4b7e276c79cea99a58b95
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-12-08 01:45:06 +0100

    Format

--- day1/Day1.hs
+++ day1/Day1.hs
@@ -18,20 +18,20 @@ sumCaloriesPerElf x (Just y : ys) = sumCaloriesPerElf (x + y) ys
 topThreeByTotalCalories :: Int -> Int -> Int -> [Int] -> (Int, Int, Int)
 topThreeByTotalCalories i j k [] = (i, j, k)
 topThreeByTotalCalories i j k (x : xs)
-    | i < x = topThreeByTotalCalories x i j xs
-    | j < x = topThreeByTotalCalories i x j xs
-    | k < x = topThreeByTotalCalories i j x xs
-    | otherwise = topThreeByTotalCalories i j k xs
+  | i < x = topThreeByTotalCalories x i j xs
+  | j < x = topThreeByTotalCalories i x j xs
+  | k < x = topThreeByTotalCalories i j x xs
+  | otherwise = topThreeByTotalCalories i j k xs
 
 sumTopThree :: (Int, Int, Int) -> Int
 sumTopThree (x, y, z) = x + y + z
 
 main :: IO ()
 main = do
-    content <- readLines "/home/aktersnurra/projects/advent-of-code-2022/day1/input"
-    let caloriesPerItem = map readMaybeInt content
-    let totalCaloriesPerElf = sumCaloriesPerElf 0 caloriesPerItem
-    let maxTotalCalories = maximum $ totalCaloriesPerElf
-    let topThree = sumTopThree $ topThreeByTotalCalories 0 0 0 totalCaloriesPerElf
-    print maxTotalCalories
-    print topThree
+  content <- readLines "/home/aktersnurra/projects/advent-of-code-2022/day1/input"
+  let caloriesPerItem = map readMaybeInt content
+  let totalCaloriesPerElf = sumCaloriesPerElf 0 caloriesPerItem
+  let maxTotalCalories = maximum $ totalCaloriesPerElf
+  let topThree = sumTopThree $ topThreeByTotalCalories 0 0 0 totalCaloriesPerElf
+  print maxTotalCalories
+  print topThree
--- day2/Day2.hs
+++ day2/Day2.hs
@@ -2,74 +2,82 @@
 
 module Main where
 
-import Text.Read
 import Data.List.Split
--- optimal strategy
--- A -> Y
--- B -> Z
--- C -> X
 
 readLines :: FilePath -> IO [String]
 readLines = fmap lines . readFile
 
-data Hand = Rock | Paper | Scissor deriving (Eq, Show, Enum, Bounded)
+data Shape = Rock | Paper | Scissor deriving (Eq, Show, Enum, Bounded)
 data Outcome = Win | Loss | Draw deriving (Eq, Show, Enum, Bounded)
 
--- splitOn :: String -> [T.Text]
--- splitOn x = splitOn " " x :: T.Text
-
-win :: Hand -> Hand
-win x = case x of Rock -> Paper
-                  Paper -> Scissor
-                  Scissor -> Rock
+win :: Shape -> Shape
+win x = case x of
+  Rock -> Paper
+  Paper -> Scissor
+  Scissor -> Rock
 
 outcomeScore :: Outcome -> Int
-outcomeScore x = case x of Win -> 6
-                           Draw -> 3
-                           Loss -> 0
+outcomeScore x = case x of
+  Win -> 6
+  Draw -> 3
+  Loss -> 0
 
-handScore :: Hand -> Int
-handScore x = case x of Rock -> 1
-                        Paper -> 2
-                        Scissor -> 3
+shapeScore :: Shape -> Int
+shapeScore x = case x of
+  Rock -> 1
+  Paper -> 2
+  Scissor -> 3
 
-score :: Hand -> Hand -> Int
-score opponent player = case (opponent,player) of (Rock,Paper) -> outcomeScore Win + handScore Paper
-                                                  (Rock,Scissor) -> outcomeScore Loss + handScore Scissor
-                                                  (Rock,Rock) -> outcomeScore Draw + handScore Rock
-                                                  (Paper,Paper) -> outcomeScore Draw + handScore Paper
-                                                  (Paper,Scissor) -> outcomeScore Win + handScore Scissor
-                                                  (Paper,Rock) -> outcomeScore Loss + handScore Rock
-                                                  (Scissor,Paper) -> outcomeScore Loss + handScore Paper
-                                                  (Scissor,Scissor) -> outcomeScore Draw + handScore Scissor
-                                                  (Scissor,Rock) -> outcomeScore Win + handScore Rock
+score :: Shape -> Shape -> Int
+score opponent player = case (opponent, player) of
+  (Rock, Paper) -> outcomeScore Win + shapeScore Paper
+  (Rock, Scissor) -> outcomeScore Loss + shapeScore Scissor
+  (Rock, Rock) -> outcomeScore Draw + shapeScore Rock
+  (Paper, Paper) -> outcomeScore Draw + shapeScore Paper
+  (Paper, Scissor) -> outcomeScore Win + shapeScore Scissor
+  (Paper, Rock) -> outcomeScore Loss + shapeScore Rock
+  (Scissor, Paper) -> outcomeScore Loss + shapeScore Paper
+  (Scissor, Scissor) -> outcomeScore Draw + shapeScore Scissor
+  (Scissor, Rock) -> outcomeScore Win + shapeScore Rock
 
-toHand :: String -> Hand
-toHand x = case x of x | elem x ["A","X"] -> Rock
-                       | elem x ["B","Y"] -> Paper
-                       | elem x ["C","Z"] -> Scissor
-                       | otherwise -> error "Non valid input"
+toShape :: String -> Shape
+toShape x
+  | elem x ["A", "X"] = Rock
+  | elem x ["B", "Y"] = Paper
+  | elem x ["C", "Z"] = Scissor
+  | otherwise = error "Non valid input"
 
 toOutcome :: String -> Outcome
-toOutcome x = case x of "X" -> Loss
-                        "Y" -> Draw
-                        "Z" -> Win
+toOutcome x = case x of
+  "X" -> Loss
+  "Y" -> Draw
+  "Z" -> Win
 
-toPlayerHand :: Outcome -> Hand -> Hand
-toPlayerHand o x = case (o,x) of (Loss,Rock) -> Scissor
-                                 (Loss,Scissor) -> Paper
-                                 (Loss,Paper) -> Rock
-                                 (Draw,Rock) -> Rock
-                                 (Draw,Scissor) -> Scissor
-                                 (Draw,Paper) -> Paper
-                                 (Win,Rock) -> Paper
-                                 (Win,Scissor) -> Rock
-                                 (Win,Paper) -> Scissor
+toPlayerShape :: Outcome -> Shape -> Shape
+toPlayerShape o x = case (o, x) of
+  (Loss, Rock) -> Scissor
+  (Loss, Scissor) -> Paper
+  (Loss, Paper) -> Rock
+  (Draw, Rock) -> Rock
+  (Draw, Scissor) -> Scissor
+  (Draw, Paper) -> Paper
+  (Win, Rock) -> Paper
+  (Win, Scissor) -> Rock
+  (Win, Paper) -> Scissor
 
 main :: IO ()
 main = do
   content <- readLines "/home/aktersnurra/projects/advent-of-code-2022/day2/input"
-  let y = sum $ map (\(x,y) -> score x y) $ map (\[x,y] -> (toHand x, toHand y)) $ map (\x -> splitOn " " x) content
+  let y =
+        sum $
+          map (\(x, y) -> score x y) $
+            map (\[x, y] -> (toShape x, toShape y)) $
+              map (\x -> splitOn " " x) content
   print y
-  let z = sum $ map (\(x,y) -> score x y) $ map (\(x,y) -> (x, toPlayerHand y x)) $ map (\[x,y] -> (toHand x, toOutcome y)) $ map (\x -> splitOn " " x) content
+  let z =
+        sum $
+          map (\(x, y) -> score x y) $
+            map (\(x, y) -> (x, toPlayerShape y x)) $
+              map (\[x, y] -> (toShape x, toOutcome y)) $
+                map (\x -> splitOn " " x) content
   print z
