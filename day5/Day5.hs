d6fda36d9fa0cf936320c31afd3439fca8eec8cb
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-12-17 01:26:42 +0100

    Add day 5 solution

--- day5/Day5.hs
+++ day5/Day5.hs
@@ -2,43 +2,76 @@ module Main where
 
 import Data.List.Split
 import qualified Data.Map as Map
-import Text.Read
+import Text.Read (readMaybe)
 
 readMaybeInt :: String -> Maybe Int
 readMaybeInt x = readMaybe x
 
-data Rearrange = Rearrange {numCrates :: Int, from :: Int, to :: Int} deriving (Show)
+data Rearrange = Rearrange {move :: Int, from :: Int, to :: Int} deriving (Show)
 
 toRearrange :: [Int] -> Rearrange
-toRearrange [x,y,z] = Rearrange x y z
+toRearrange [x, y, z] = Rearrange x y z
 
 parseRearrangement :: [String] -> Rearrange
-parseRearrangement xs = toRearrange $ [x | Just x <-[readMaybeInt x | x <- xs]]
+parseRearrangement xs = toRearrange $ [x | Just x <- [readMaybeInt x | x <- xs]]
 
 readLines :: FilePath -> IO [String]
 readLines = fmap lines . readFile
 
 parseLayer :: String -> [String]
 parseLayer [] = []
-parseLayer xs = (take 3 xs):(parseLayer (drop 4 xs))
+parseLayer xs = (filter (`elem` ['A' .. 'Z']) $ (take 3 xs)) : (parseLayer (drop 4 xs))
 
-addCrate :: [v] -> [v] -> [v]
-addCrate newCrate crates = crates ++ newCrate
+insert :: Int -> [v] -> Map.Map Int [v] -> Map.Map Int [v]
+insert i stack stacks = Map.insert i stack stacks
 
-removeCrate :: [v] -> [v]
-removeCrate xs = drop 1 xs
+get :: Int -> Map.Map Int [v] -> [v]
+get i stacks = stacks Map.! i
+
+pop :: Rearrange -> Map.Map Int [v] -> ([v], Map.Map Int [v])
+pop x y = (pop, updatedStack)
+ where
+  stack = get (from x) y
+  pop = drop ((length stack) - (move x)) stack
+  keep = take ((length stack) - (move x)) stack
+  updatedStack = insert (from x) keep y
+
+push :: Rearrange -> [v] -> Map.Map Int [v] -> Map.Map Int [v]
+push x y z = updatedStacks
+ where
+  stack = get (to x) z
+  updated = (++) stack y
+  updatedStacks = insert (to x) updated z
+
+moveCrates :: Rearrange -> Map.Map Int [v] -> Map.Map Int [v]
+moveCrates x y = updated
+ where
+  (j, k) = pop x y
+  updated = push x (reverse j) k
+
+moveCrates9001 :: Rearrange -> Map.Map Int [v] -> Map.Map Int [v]
+moveCrates9001 x y = updated
+ where
+  (j, k) = pop x y
+  updated = push x j k
+
+topCrates :: Map.Map Int [v] -> [v]
+topCrates x = y
+ where
+  y = map (\k -> (last $ get k x)) [1 .. 9]
 
 main :: IO ()
 main = do
-  -- content <- readLines "/home/aktersnurra/projects/advent-of-code-2022/day5/input"
-  -- let rowsOfCrates = filter (\(_,y) -> y /= "   ") $ concat $ map (\x -> zip [1..] x) $ map parseLayer $ take 8 content
-  -- print rowsOfCrates
-  -- let crates = Map.fromListWith (++) [ (k, [v]) | (k, v) <- rowsOfCrates]
-  -- print crates
-  -- let cmds = map parseRearrangement $ map (splitOn " ") $ drop 10 content
-  -- print cmds
-  let x = Map.singleton 1 ["a", "b"]
-  -- let z = Map.insertWith addCrate 1 ["c"] z
-  print x
-  let y = Map.adjust removeCrate 1 x
+  content <- readLines "/home/aktersnurra/projects/advent-of-code-2022/day5/input"
+  let rowsOfCrates = filter (\(_, y) -> y /= "") $ concat $ map (\x -> zip [1 ..] x) $ map parseLayer $ take 8 content
+  let stacks = Map.fromListWith (++) [(k, [v]) | (k, v) <- rowsOfCrates]
+  let rearrangements = map parseRearrangement $ map (splitOn " ") $ drop 10 content
+  let finalStack = foldl (\acc x -> moveCrates x acc) stacks rearrangements
+  print finalStack
+  let y = concat $ topCrates finalStack
   print y
+
+  let finalStack9001 = foldl (\acc x -> moveCrates9001 x acc) stacks rearrangements
+  print finalStack9001
+  let z = concat $ topCrates finalStack9001
+  print z
